d250e3108b2482a65ae6dd43c892688259787311
Svetoslav Dekov <scdekov@gmail.com>
2020-04-26 12:28:48 +0300

    Init react app

---
+++ app/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './components/App';
+
+const root = document.createElement('div');
+root.setAttribute('id', 'root');
+document.body.appendChild(root);
+ReactDOM.render(
+  <App />,
+  root
+);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  entry: [
+    path.resolve(ROOT_PATH, 'app/src/index'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'app/build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(ROOT_PATH, 'app/build'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Weather dashboard'
+    })
+  ]
+};
