9c465f7fb9b02becec2b6c148914f2538283ddc7
Svetoslav Dekov <scdekov@gmail.com>
2020-04-26 13:22:22 +0300

    Add register basic UI

--- app/src/components/Auth.jsx
+++ app/src/components/Auth.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { API_URL } from '../config';
 
-export const Auth = ({ setAuthenticated }) => {
+const Login = ({ setAuthenticated }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = async e => {
     e.preventDefault();
-    const resp = await axios.post('http://localhost:3000/api/v1/login', { username, password });
+    const resp = await axios.post(`${API_URL}/login`, { username, password });
     if (resp.status === 200) {
       setAuthenticated(true);
     }
@@ -20,6 +21,46 @@ export const Auth = ({ setAuthenticated }) => {
         <input type="password" onChange={e => setPassword(e.target.value)} value={password}/>
         <input type="submit" value="Login"/>
       </form>
+    </div>
+  );
+};
+
+const Register = ({ onRegisterSuccess }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async e => {
+    e.preventDefault();
+    const resp = await axios.post(`${API_URL}/register`, { username, password });
+    if (resp.status === 200) {
+      onRegisterSuccess();
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleRegister}>
+        <input type="text" onChange={e => setUsername(e.target.value)} value={username}/>
+        <input type="password" onChange={e => setPassword(e.target.value)} value={password}/>
+        <input type="submit" value="Register"/>
+      </form>
+    </div>
+  );
+};
+
+export const Auth = ({ setAuthenticated }) => {
+  const [activePage, setActivePage] = useState('login');
+
+  return (
+    <div>
+      {
+        activePage === 'login' ?
+        <div>
+          <Login setAuthenticated={setAuthenticated}/>
+          <button onClick={() => setActivePage('register')}>Register</button>
+        </div>:
+        <Register onRegisterSuccess={() => setActivePage('login')}/>
+      }
     </div>
   );
 };
